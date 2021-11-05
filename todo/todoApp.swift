1699d5c82e6a49b9172f4af7f1632d3371b85d57
Wender <outrowender@gmail.com>
2021-11-05 14:11:43 -0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Wender Cruz on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
