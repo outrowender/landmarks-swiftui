a33e9d58cd3a5b30fc88fdb01bc77301ac3704ca
Wender <outrowender@gmail.com>
2021-11-22 19:13:36 -0300

    adicionadas novas formas

---
+++ landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  landmarks
+//
+//  Created by Wender Patrick on 22/11/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+                    .padding(-60)
+                    .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
