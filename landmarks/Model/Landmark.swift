72e7045c00215c439a6f658b6716e913811dc650
Wender <outrowender@gmail.com>
2021-11-23 01:49:29 -0300

    adicionado pageviewcontroller

--- landmarks/Model/Landmark.swift
+++ landmarks/Model/Landmark.swift
@@ -19,25 +19,29 @@ struct Landmark: Hashable, Codable, Identifiable {
     var isFeatured: Bool
     
     var category: Category
-    enum Category: String, CaseIterable, Codable {
-        case lakes = "Lakes"
-        case rivers = "Rivers"
-        case mountains = "Mountains"
-    }
-    
     private var imageName: String
+    private var coordinates: Coordinates
+    
     var image: Image {
         Image(imageName)
     }
     
-    private var coordinates: Coordinates
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
     
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
-    
+
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
